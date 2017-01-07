daab15d0de3ce2b842a58ba292363afade896539
yuanjinlin <linyj@ecallcen.com>
2017-01-07 11:19:09 +0800

    typo

--- src/common/Que.h
+++ src/common/Que.h
@@ -5,7 +5,7 @@
 name[name ## _cursor] = val; \
 name ## _cursor++; \
 if (name ## _cursor == name ## _len) { \
-    name ## _cursor = 0;\
+    name ## _cursor = 0; \
 }
 
 #define Que_get(name, idx) name[(name ## _cursor + idx) % name ## _len]
