7c696854e926fd188c0873083e1e37e3326a6bd4
yuanjinlin <linyj@ecallcen.com>
2017-01-08 01:29:14 +0800

    api set

--- src/proj/PiXiuStr.h
+++ src/proj/PiXiuStr.h
@@ -1,13 +1,24 @@
+#include "../common/gen.h"
 #include <stdint.h>
 
-#define CHUNK_PXS_NUM 65535
+#define CHUNK_STR_NUM 65535
 
 struct PiXiuStr {
     uint16_t len;
     uint8_t data[];
+
+    Generator parse(int, int, PiXiuChunk *);
+
+    PiXiuStr * concat(PiXiuStr *);
 };
 
 struct PiXiuChunk {
-    PiXiuStr * strs[CHUNK_PXS_NUM];
-    int used_num;
+    PiXiuStr * strs[CHUNK_STR_NUM];
+    int used_num = 0;
 };
+
+PiXiuStr * PiXiuStr_init_k(uint8_t[], int);
+
+PiXiuStr * PiXiuStr_init(uint8_t[], int);
+
+PiXiuStr * PiXiuStr_init_stream(uint8_t, int, int);
