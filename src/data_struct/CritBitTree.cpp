9d6d3150336e1dc5e61ceeb4c1a2dacbcd1c43f6
yuanjinlin <linyj@ecallcen.com>
2017-01-11 11:24:49 +0800

    CBT setitem

--- src/data_struct/CritBitTree.cpp
+++ src/data_struct/CritBitTree.cpp
@@ -3,6 +3,17 @@
 #include <stddef.h>
 
 int CritBitTree::setitem(PiXiuStr * src, PiXiuChunk * ctx, uint16_t chunk_idx) {
+    if (this->root == NULL) {
+        this->root = ctx;
+        this->chunk_idx = chunk_idx;
+    } else {
+        auto ret = this->find_best_match(src);
+        auto grand = (CBTInner *) ret.grand;
+        auto pa = (CBTInner *) ret.pa;
+        auto crit_node = (PiXiuChunk *) ret.crit_node;
+
+        uint16_t diff_at = 0;
+    }
 }
 
 char * CritBitTree::repr(void) {
@@ -19,10 +30,11 @@ CritBitTree::fbm_ret CritBitTree::find_best_match(PiXiuStr * src) {
 
         uint8_t crit_byte = src->len > inner->diff_at ? src->data[inner->diff_at] : (uint8_t) 0;
         uint8_t direct = ((uint8_t) 1 + (inner->mask | crit_byte)) >> 8;
-        Que_push(q, cursor);
         cursor = inner->crit_node[direct];
+        Que_push(q, cursor);
     }
-    return CritBitTree::fbm_ret{.grand=Que_get(q, 0), .pa=Que_get(q, 1), .crit_node=Que_get(q, 2)};
+    return CritBitTree::fbm_ret
+            {adr_de_spec(Que_get(q, 0)), adr_de_spec(Que_get(q, 1)), adr_de_spec(Que_get(q, 2))};
 }
 
 CBTInner * CBTInner_init(void) {
