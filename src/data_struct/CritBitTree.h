df417fc98f1ef91eb3d0db2c45ae1607c3bade63
yuanjinlin <linyj@ecallcen.com>
2017-01-14 12:48:29 +0800

    easy part ok

--- src/data_struct/CritBitTree.h
+++ src/data_struct/CritBitTree.h
@@ -1,6 +1,7 @@
 #ifndef CRIT_BIT_TREE_H
 #define CRIT_BIT_TREE_H
 
+#include "../common/List.h"
 #include "../proj/PiXiuStr.h"
 
 #define CBT_SET_REPLACE 1
@@ -53,13 +54,23 @@ $gen(CBTGen) {
     CritBitTree * self;
     PiXiuStr * prefix;
 
+    static void * node_sub(CBTInner * node, uint8_t direct) {
+        auto ptr = node->crit_node_arr[direct];
+        if (adr_is_spec(ptr)) {
+            return ptr;
+        } else {
+            auto chunk = (PiXiuChunk *) ptr;
+            return chunk->getitem(node->chunk_idx_arr[direct]);
+        }
+    }
+
     $emit(PiXiuStr *)
             auto ret = self->find_best_match(prefix);
             auto pa = (CBTInner *) ret.pa;
             auto chunk = (PiXiuChunk *) ret.crit_node;
-            uint8_t pa_direct = ret.pa_direct;
+            auto pa_direct = ret.pa_direct;
 
-            int chunk_idx = self->chunk_idx;
+            auto chunk_idx = self->chunk_idx;
             if (pa != NULL) {
                 chunk_idx = pa->chunk_idx_arr[pa_direct];
             }
@@ -69,7 +80,23 @@ $gen(CBTGen) {
                 $yield(pxs);
 
                 if (pa_direct != 1) {
+                    List_init(void *, q);
+                    List_append(void *, q, this->node_sub(pa, 1));
 
+                    while (q_len) {
+                        auto cursor = q[q_len - 1];
+                        List_del(void *, q, q_len - 1);
+
+                        if (!adr_is_spec(cursor)) {
+                            $yield((PiXiuStr *) cursor);
+                        } else {
+                            auto inner = (CBTInner *) cursor;
+                            List_append(void *, q, this->node_sub(inner, 1));
+                            List_append(void *, q, this->node_sub(inner, 0));
+                        }
+                    }
+
+                    List_free(q);
                 }
             }
     $stop;
