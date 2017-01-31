f1b2a578ae656a0eece8f3af3ce714894b2d1596
yuanjinlin <linyj@ecallcen.com>
2017-01-31 13:34:44 +0800

    pxc crud test

--- src/proj/PiXiuCtrl.cpp
+++ src/proj/PiXiuCtrl.cpp
@@ -1,6 +1,8 @@
 #include "PiXiuCtrl.h"
+#include <map>
+#include <string>
 
-#define REINSERT_FACTOR 0.5
+#define REINSERT_RATE 0.5
 
 extern PiXiuChunk * Glob_Reinsert_Chunk;
 
@@ -10,7 +12,7 @@ int PiXiuCtrl::setitem(uint8_t k[], int k_len, uint8_t v[], int v_len, bool rein
         this->st.free_prop();
         this->st.init_prop();
 
-        if (last_chunk->used_num < REINSERT_FACTOR * PXC_STR_NUM) {
+        if (last_chunk->used_num < REINSERT_RATE * PXC_STR_NUM) {
             if (last_chunk == Glob_Reinsert_Chunk) {
                 Glob_Reinsert_Chunk = NULL;
             }
@@ -26,6 +28,7 @@ int PiXiuCtrl::setitem(uint8_t k[], int k_len, uint8_t v[], int v_len, bool rein
         auto pxs_k = PiXiuStr_init_key(k, k_len);
         auto pxs_v = PiXiuStr_init_key(v, v_len);
         doc = pxs_k->concat(pxs_v);
+        doc->data[doc->len - 1] = PXS_KEY_SEC;
         PiXiuStr_free(pxs_k);
         PiXiuStr_free(pxs_v);
     } else if (!k_len && !v_len) {
@@ -78,7 +81,7 @@ void PiXiuCtrl::free_prop() {
 }
 
 void PiXiuCtrl::reinsert(PiXiuChunk *& chunk) {
-    assert(chunk->used_num < REINSERT_FACTOR * PXC_STR_NUM);
+    assert(chunk->used_num < REINSERT_RATE * PXC_STR_NUM);
     auto glob_chunk = Glob_Reinsert_Chunk;
 
     for (int i = 0; i < lenOf(chunk->strs); ++i) {
@@ -106,17 +109,19 @@ void PiXiuCtrl::reinsert(PiXiuChunk *& chunk) {
 }
 
 void t_PiXiuCtrl(void) {
-    PiXiuCtrl ctrl;
+    using namespace std;
+    srand(19950207);
+    PiXiuCtrl pxc;
 
     // <max len elem>
-    ctrl.init_prop();
+    pxc.init_prop();
 
     uint8_t max_elem[PXSG_MAX_TO - 2];
     max_elem[0] = 233;
     for (int i = 1; i < lenOf(max_elem); ++i) max_elem[i] = 1;
-    ctrl.setitem(max_elem, lenOf(max_elem), NULL, 0);
+    pxc.setitem(max_elem, lenOf(max_elem), NULL, 0);
 
-    auto gen = ctrl.getitem(max_elem, lenOf(max_elem));
+    auto gen = pxc.getitem(max_elem, lenOf(max_elem));
     auto j = 0;
     uint8_t rv;
     while (gen->operator()(rv)) {
@@ -139,29 +144,53 @@ void t_PiXiuCtrl(void) {
     assert(j == PXSG_MAX_TO);
     PXSGen_free(gen);
 
-    ctrl.free_prop();
+    pxc.free_prop();
     // </>
 
     // <max len kv>
-    ctrl.init_prop();
+    pxc.init_prop();
 
     uint8_t max_k[100];
     uint8_t max_v[PXSG_MAX_TO - (lenOf(max_k) + 2) - 2];
     for (int i = 0; i < lenOf(max_k); ++i) max_k[i] = 6;
     for (int i = 0; i < lenOf(max_v); ++i) max_v[i] = 2;
-    ctrl.setitem(max_k, lenOf(max_k), max_v, lenOf(max_v));
+    pxc.setitem(max_k, lenOf(max_k), max_v, lenOf(max_v));
 
-    gen = ctrl.getitem(max_k, lenOf(max_k));
+    gen = pxc.getitem(max_k, lenOf(max_k));
     j = 0;
     while (gen->operator()(rv)) {
         if (0 <= j && j <= lenOf(max_k) - 1) { assert(rv == 6); }
         else { assert(rv != 6); }
         j++;
     }
-    assert(j == PXSG_MAX_TO);
     PXSGen_free(gen);
 
-    ctrl.free_prop();
+    pxc.free_prop();
+    // </>
+
+    // <CRUD>
+    pxc.init_prop();
+
+    string alphabet[] = {"A", "B", "C", "D", "E"};
+    map<string, string> ctrl_group;
+    for (int i = 0; i < PXC_STR_NUM * 1.5; ++i) {
+        string sample_k;
+        auto len = rand() % 50 + 1;
+        for (int k = 0; k < len; ++k) {
+            sample_k += alphabet[rand() % lenOf(alphabet)];
+        }
+        sample_k += '.';
+        string sample_v;
+        len = rand() % 50 + 1;
+        for (int k = 0; k < len; ++k) {
+            sample_v += alphabet[rand() % lenOf(alphabet)];
+        }
+        sample_v += '.';
+        pxc.setitem((uint8_t *) sample_k.c_str(), (int) sample_k.size(),
+                    (uint8_t *) sample_v.c_str(), (int) sample_v.size());
+    }
+
+    pxc.free_prop();
     // </>
 
     PRINT_FUNC;
